bc027518fb875dad4f7d12c6927a80721865f365
TaniaTatarchina <zinya111@yahoo.com>
2019-05-15 13:43:03 +0300

    Finished project

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('account/', include('account.urls')),
+    path('products/', include('products.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
